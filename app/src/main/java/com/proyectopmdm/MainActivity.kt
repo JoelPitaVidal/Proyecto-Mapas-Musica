722b4def2450e101125973efaa05ffb8836b881c
JoelPitaVidal <jpitavidal@daielcastelao.org>
2024-12-04 18:14:15 +0100

    Início del Proyecto

---
+++ app/src/main/java/com/proyectopmdm/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.proyectopmdm
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+//Style URL mapbox : mapbox://styles/joelpitavidal/cm42zsuoi006e01s6ad5l4nt7
+//Access token : sk.eyJ1Ijoiam9lbHBpdGF2aWRhbCIsImEiOiJjbTQ1a2Rtd3cwbG4yMmtzNDJ3ZWUwZ2I1In0.RHJ994dwjALgNRlU-c5gZg
